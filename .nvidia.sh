291296c51897629753271fdf7270540a5d68b43a
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2022-01-14 10:18:03 -0500

    Clean root directory

--- .nvidia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if cat /proc/bus/pci/devices | grep vfio &>/dev/null; then
-    echo "OFF"
-else
-    nvidia-settings -query GPUCoreTemp -t | head -n 1 | tr -d '\n'
-    echo " C"
-fi
